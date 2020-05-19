29a0d85f94fd815e7fd2aa65594d1b3c63b6d712
Jan Vernieuwe <jan.vernieuwe@phpro.be>
2020-05-19 13:07:15 +0200

    Debugging

--- Middleware/NewRelicMiddleware.php
+++ Middleware/NewRelicMiddleware.php
@@ -37,7 +37,10 @@ class NewRelicMiddleware implements MiddlewareInterface
         $this->newrelicManager->startTransaction();
         // Accept the stamp
         if ($stamp = $envelope->last(TraceStamp::class)) {
+            printf('addinf trace data %s', $stamp->getTraceData());
             $this->newrelicManager->acceptTrace($stamp->getTraceData());
+        }else{
+            print "No trace stamp found\n";
         }
         $this->newrelicManager->nameTransaction($this->newrelicTransactionNameManager->getTransactionName($envelope));
         try {
