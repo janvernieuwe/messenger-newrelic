41c16359a65565cca66ea78c88009ed3b37590a6
Jan Vernieuwe <jan.vernieuwe@phpro.be>
2020-05-20 10:34:27 +0200

    Revert "Refactor tests"
    This reverts commit 25ed9653

--- Middleware/NewRelicMiddleware.php
+++ Middleware/NewRelicMiddleware.php
@@ -4,6 +4,7 @@ namespace Arxus\NewrelicMessengerBundle\Middleware;
 
 use Arxus\NewrelicMessengerBundle\Newrelic\NewrelicManager;
 use Arxus\NewrelicMessengerBundle\Newrelic\NewrelicTransactionNameManager;
+use Arxus\NewrelicMessengerBundle\Stamp\TraceStamp;
 use Symfony\Component\Messenger\Envelope;
 use Symfony\Component\Messenger\Exception\HandlerFailedException;
 use Symfony\Component\Messenger\Middleware\MiddlewareInterface;
@@ -21,10 +22,8 @@ class NewRelicMiddleware implements MiddlewareInterface
      */
     private $newrelicTransactionNameManager;
 
-    public function __construct(
-        NewrelicManager $newrelicManager,
-        NewrelicTransactionNameManager $newrelicTransactionNameManager
-    ) {
+    public function __construct(NewrelicManager $newrelicManager, NewrelicTransactionNameManager $newrelicTransactionNameManager)
+    {
         $this->newrelicManager = $newrelicManager;
         $this->newrelicTransactionNameManager = $newrelicTransactionNameManager;
     }
@@ -32,20 +31,25 @@ class NewRelicMiddleware implements MiddlewareInterface
     public function handle(Envelope $envelope, StackInterface $stack): Envelope
     {
         if (!$this->newrelicManager->isEnabled()) {
-            return $envelope;
+            return $stack->next()->handle($envelope, $stack);
         }
 
         $this->newrelicManager->startTransaction();
+        // Accept the stamp
+        if ($stamp = $envelope->last(TraceStamp::class)) {
+            printf('addinf trace data %s', json_encode($stamp->getTraceData()));
+            $this->newrelicManager->acceptTrace($stamp->getTraceData());
+        }else{
+            print "No trace stamp found\n";
+        }
         $this->newrelicManager->nameTransaction($this->newrelicTransactionNameManager->getTransactionName($envelope));
         try {
-            $stack->next()->handle($envelope, $stack);
+            return $stack->next()->handle($envelope, $stack);
         } catch (HandlerFailedException $e) {
             $this->newrelicManager->noticeError($e);
             throw $e;
         } finally {
             $this->newrelicManager->endTransaction();
-
-            return $envelope;
         }
     }
 }
--- tests/Listener/ConsoleCommandListenerTest.php
+++ tests/Listener/ConsoleCommandListenerTest.php
@@ -8,8 +8,6 @@ use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Event\ConsoleCommandEvent;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 
 class ConsoleCommandListenerTest extends TestCase
 {
@@ -32,20 +30,16 @@ class ConsoleCommandListenerTest extends TestCase
     {
         // Create mocks
         $this->commandMock = $this->createMock(Command::class);
-        $this->eventMock = new ConsoleCommandEvent(
-            $this->command = new Command('messenger:consume'),
-            $this->createMock(InputInterface::class),
-            $this->createMock(OutputInterface::class),
-        );
+        $this->eventMock = $this->createMock(ConsoleCommandEvent::class);
         $this->newrelicManagerMock = $this->createMock(NewrelicManager::class);
     }
 
     public function testInvokeEnabled(): void
     {
-//        $this->commandMock
-//            ->expects($this->once())
-//            ->method('getName')
-//            ->willReturn('messenger:consume');
+        $this->commandMock
+            ->expects($this->once())
+            ->method('getName')
+            ->willReturn('messenger:consume');
         $this->newrelicManagerMock
             ->expects($this->once())
             ->method('endTransaction');
@@ -53,10 +47,10 @@ class ConsoleCommandListenerTest extends TestCase
             ->expects($this->once())
             ->method('isEnabled')
             ->willReturn(true);
-//        $this->eventMock
-//            ->expects($this->once())
-//            ->method('getCommand')
-//            ->willReturn($this->commandMock);
+        $this->eventMock
+            ->expects($this->once())
+            ->method('getCommand')
+            ->willReturn($this->commandMock);
         $commandListener = new ConsoleCommandListener($this->newrelicManagerMock);
         $commandListener($this->eventMock);
     }
@@ -74,21 +68,20 @@ class ConsoleCommandListenerTest extends TestCase
             ->expects($this->once())
             ->method('isEnabled')
             ->willReturn(false);
-//        $this->eventMock
-//            ->expects($this->once())
-//            ->method('getCommand')
-//            ->willReturn($this->commandMock);
+        $this->eventMock
+            ->expects($this->once())
+            ->method('getCommand')
+            ->willReturn($this->commandMock);
         $commandListener = new ConsoleCommandListener($this->newrelicManagerMock);
         $commandListener($this->eventMock);
     }
 
     public function testInvokeOtherCommand(): void
     {
-        $this->eventMock = new ConsoleCommandEvent(
-            $this->command = new Command('other:command'),
-            $this->createMock(InputInterface::class),
-            $this->createMock(OutputInterface::class),
-        );
+        $this->commandMock
+            ->expects($this->once())
+            ->method('getName')
+            ->willReturn('other:command');
         $this->newrelicManagerMock
             ->expects($this->never())
             ->method('endTransaction');
@@ -96,32 +89,31 @@ class ConsoleCommandListenerTest extends TestCase
             ->expects($this->once())
             ->method('isEnabled')
             ->willReturn(true);
-//        $this->eventMock
-//            ->expects($this->once())
-//            ->method('getCommand')
-//            ->willReturn($this->commandMock);
+        $this->eventMock
+            ->expects($this->once())
+            ->method('getCommand')
+            ->willReturn($this->commandMock);
         $commandListener = new ConsoleCommandListener($this->newrelicManagerMock);
         $commandListener($this->eventMock);
     }
 
     public function testInvokeNoCommand(): void
     {
-        $this->eventMock = new ConsoleCommandEvent(
-            $this->command = new Command('other:command'),
-            $this->createMock(InputInterface::class),
-            $this->createMock(OutputInterface::class),
-        );
+        $this->commandMock
+            ->expects($this->never())
+            ->method('getName')
+            ->willReturn('other:command');
         $this->newrelicManagerMock
             ->expects($this->never())
             ->method('endTransaction');
         $this->newrelicManagerMock
-            ->expects($this->once())
+            ->expects($this->never())
             ->method('isEnabled')
             ->willReturn(true);
-//        $this->eventMock
-//            ->expects($this->once())
-//            ->method('getCommand')
-//            ->willReturn('');
+        $this->eventMock
+            ->expects($this->once())
+            ->method('getCommand')
+            ->willReturn('');
         $commandListener = new ConsoleCommandListener($this->newrelicManagerMock);
         $commandListener($this->eventMock);
     }
