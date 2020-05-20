79505a4310c4fdd6c1662fcf692dc7e52717b7b1
Jan Vernieuwe <jan.vernieuwe@phpro.be>
2020-05-20 10:32:10 +0200

    Undo changes

--- Middleware/NewRelicMiddleware.php
+++ Middleware/NewRelicMiddleware.php
@@ -4,7 +4,6 @@ namespace Arxus\NewrelicMessengerBundle\Middleware;
 
 use Arxus\NewrelicMessengerBundle\Newrelic\NewrelicManager;
 use Arxus\NewrelicMessengerBundle\Newrelic\NewrelicTransactionNameManager;
-use Arxus\NewrelicMessengerBundle\Stamp\TraceStamp;
 use Symfony\Component\Messenger\Envelope;
 use Symfony\Component\Messenger\Exception\HandlerFailedException;
 use Symfony\Component\Messenger\Middleware\MiddlewareInterface;
@@ -22,8 +21,10 @@ class NewRelicMiddleware implements MiddlewareInterface
      */
     private $newrelicTransactionNameManager;
 
-    public function __construct(NewrelicManager $newrelicManager, NewrelicTransactionNameManager $newrelicTransactionNameManager)
-    {
+    public function __construct(
+        NewrelicManager $newrelicManager,
+        NewrelicTransactionNameManager $newrelicTransactionNameManager
+    ) {
         $this->newrelicManager = $newrelicManager;
         $this->newrelicTransactionNameManager = $newrelicTransactionNameManager;
     }
@@ -31,25 +32,20 @@ class NewRelicMiddleware implements MiddlewareInterface
     public function handle(Envelope $envelope, StackInterface $stack): Envelope
     {
         if (!$this->newrelicManager->isEnabled()) {
-            return $stack->next()->handle($envelope, $stack);
+            return $envelope;
         }
 
         $this->newrelicManager->startTransaction();
-        // Accept the stamp
-        if ($stamp = $envelope->last(TraceStamp::class)) {
-            printf('addinf trace data %s', json_encode($stamp->getTraceData()));
-            $this->newrelicManager->acceptTrace($stamp->getTraceData());
-        }else{
-            print "No trace stamp found\n";
-        }
         $this->newrelicManager->nameTransaction($this->newrelicTransactionNameManager->getTransactionName($envelope));
         try {
-            return $stack->next()->handle($envelope, $stack);
+            $stack->next()->handle($envelope, $stack);
         } catch (HandlerFailedException $e) {
             $this->newrelicManager->noticeError($e);
             throw $e;
         } finally {
             $this->newrelicManager->endTransaction();
+
+            return $envelope;
         }
     }
 }
